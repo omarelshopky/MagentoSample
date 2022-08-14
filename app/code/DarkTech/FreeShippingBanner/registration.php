554739c6a46be4c05bc9c4679e7a983bd0aaedf0
Omar Elshopky <omar.elshopky202@gmail.com>
2022-08-14 03:21:18 +0200

    Task 2: Insert a new container and a “Free Shipping” block

---
+++ app/code/DarkTech/FreeShippingBanner/registration.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'DarkTech_FreeShippingBanner',
+    __DIR__
+);
